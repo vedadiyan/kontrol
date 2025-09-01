e076e22aef2505b22c87520878db5d9c64b26bce
Pouya Vedadiyan <vedadiyan@gmail.com>
2025-09-01 18:04:36 +0800

    inspector

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/vedadiyan/kontrol
 
 go 1.23.0
 
-require github.com/google/uuid v1.6.0
+require github.com/vedadiyan/exql v0.0.1-alpha
 
-require github.com/vedadiyan/exql v0.0.0-20250822080224-4ac4bc823f1a // indirect
+require github.com/google/uuid v1.6.0 // indirect
--- internal/filters/inspector/inspector.go
+++ internal/filters/inspector/inspector.go
@@ -17,24 +17,27 @@ package inspector
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 
-	eval "github.com/vedadiyan/exql/lang"
+	"github.com/vedadiyan/exql"
+	"github.com/vedadiyan/exql/lang"
 	"github.com/vedadiyan/kontrol/internal/pipeline"
 )
 
 type InspectorFilter struct {
 	pipeline.FilterWrapper
-	Query string
-}
-
-type Context struct {
+	expr lang.ExprNode
 }
 
 func New(id string, query string, opts ...pipeline.FilterOption) *InspectorFilter {
 	filter := new(InspectorFilter)
 	filter.FilterId = id
-	filter.Query = query
+	expr, err := exql.Parse(query)
+	if err != nil {
+		panic(err)
+	}
+	filter.expr = expr
 	filter.Handler(filter.Do)
 
 	for _, opt := range opts {
@@ -45,13 +48,10 @@ func New(id string, query string, opts ...pipeline.FilterOption) *InspectorFilte
 }
 
 func (f *InspectorFilter) Do(ctx context.Context, responseNode pipeline.ResponseNode, request *http.Request) error {
-	expr, err := eval.ParseExpression(f.Query)
+	result, err := f.expr.Evaluate(exql.NewDefaultContext())
 	if err != nil {
 		return f.HandleError(ctx, responseNode, request, err)
 	}
-
-	context := new(Context)
-	result := expr.Evaluate(context)
 	boolean, ok := result.(bool)
 	if !ok {
 		return f.HandleError(ctx, responseNode, request, fmt.Errorf("expected boolean but got %T", result))
@@ -62,9 +62,31 @@ func (f *InspectorFilter) Do(ctx context.Context, responseNode pipeline.Response
 	return f.HandleNext(ctx, responseNode, request)
 }
 
-func (c *Context) GetVariable(name string) eval.Value {
-	return nil
-}
-func (c *Context) GetFunction(name string) eval.Function {
-	return nil
+func createContext(response pipeline.ResponseNode, request *http.Request) exql.DefaultContext {
+	ctx := exql.NewDefaultContext()
+	rs := exql.NewDefaultContext()
+	rs.SetVariable("body", response.Current().Body)
+	rs.SetVariable("headers", response.Current().Header)
+	rs.SetVariable("statusCode", response.Current().StatusCode)
+	rs.SetVariable("trailers", response.Current().Trailer)
+	rq := exql.NewDefaultContext()
+	body, err := request.GetBody()
+	if err != nil {
+	}
+	data, err := io.ReadAll(body)
+	if err != nil {
+	}
+	rq.SetVariable("body", data)
+	rq.SetVariable("headers", request.Header)
+	rq.SetVariable("host", request.Host)
+	rq.SetVariable("method", request.Method)
+	rq.SetVariable("pattern", request.Pattern)
+	rq.SetVariable("form", request.PostForm)
+	rq.SetVariable("protoMajor", request.ProtoMajor)
+	rq.SetVariable("protoMinor", request.ProtoMinor)
+	rq.SetVariable("remoteAddr", request.RemoteAddr)
+	rq.SetVariable("trailer", request.Trailer)
+	rq.SetVariable("transferEncoding", request.TransferEncoding)
+	rq.SetVariable("url", request.URL)
+	return *ctx
 }
