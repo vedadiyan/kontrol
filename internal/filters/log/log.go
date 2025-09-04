98e0919d9cf63a46fa6561e58cc7c2c52a988bfe
Pouya Vedadiyan <vedadiyan@gmail.com>
2025-09-04 17:16:12 +0800

    log

---
+++ internal/filters/log/log.go
@@ -0,0 +1,79 @@
+// Copyright (c) 2025 Pouya Vedadiyan. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package log
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/vedadiyan/exql"
+	"github.com/vedadiyan/exql/lang"
+	_http "github.com/vedadiyan/exql/lib/http"
+	"github.com/vedadiyan/kontrol/internal/pipeline"
+)
+
+type LogFilter struct {
+	pipeline.FilterWrapper
+	expr lang.ExprNode
+}
+
+func New(id string, query string, opts ...pipeline.FilterOption) *LogFilter {
+	filter := new(LogFilter)
+	filter.FilterId = id
+	expr, err := exql.Parse(query)
+	if err != nil {
+		panic(err)
+	}
+	filter.expr = expr
+	filter.Handler(filter.Do)
+
+	for _, opt := range opts {
+		opt(&filter.FilterOptions)
+	}
+
+	return filter
+}
+
+func (f *LogFilter) Do(ctx context.Context, responseNode pipeline.ResponseNode, request *http.Request) error {
+	result, err := f.expr.Evaluate(createContext(responseNode, request))
+	switch err {
+	case nil:
+		{
+			log.Println(result)
+		}
+	default:
+		{
+			log.Println(err)
+		}
+	}
+
+	return f.HandleNext(ctx, responseNode, request)
+}
+
+func createContext(response pipeline.ResponseNode, request *http.Request) lang.Context {
+	ctx := exql.NewDefaultContext()
+	currentResponse := response.Current()
+	res := new(http.Response)
+	res.Body = io.NopCloser(bytes.NewBuffer(currentResponse.Body))
+	res.StatusCode = currentResponse.StatusCode
+	res.Header = currentResponse.Header
+	res.Trailer = currentResponse.Trailer
+	ctx.SetVariable("request", _http.New(request))
+	ctx.SetVariable("response", _http.New(res))
+	return ctx
+}
